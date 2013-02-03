1a32ad21ce5706114123b19f05dbda86976715da
C2dan88 <wildteen88@googlemail.com>
2013-02-03 19:45:09 +0000

    Added functionality to collect name and email through stdClass and display it onscreen

--- views/signup.php
+++ views/signup.php
@@ -1,7 +1,10 @@
 	<h2>Signup Now!</h2>
-	
+
 	<form action="<?php echo $this->make_route('/signup'); ?>" method="post">
 		<label for="name">Name</label>
-		<input id="name" name="name" type="text" />
+		<input id="name" name="name" type="text" /> <br />
+
+		<label for="name">Email</label>
+		<input id="email" name="email" type="text" /> <br />
 		<input type="submit" value="Submit" />
 	</form>
