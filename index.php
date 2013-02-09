19da2098a05ddf696c65d4b9703e3dd244e4a833
C2dan88 <wildteen88@googlemail.com>
2013-02-09 14:35:43 +0000

    Added a post class, built out basic post creation into the user profile.

--- index.php
+++ index.php
@@ -56,6 +56,21 @@ get('/user/:username', function($app) {
 	$app->render('user/profile');
 });
 
+post('/post', function($app) {
+	if(User::is_authenticated())
+	{
+		$post = new Post();
+		$post->content = $app->form('content');
+		$post->create();
+		$app->redirect('/user/' . User::current_user());
+	}
+	else
+	{
+		$app->set('error', 'You must be logged in to do that.');
+		$app->render('user/login');
+	}
+});
+
 resolve();
 
 /*echo '<hr /><pre>' . print_r([
